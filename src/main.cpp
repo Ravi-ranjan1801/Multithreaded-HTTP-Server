25352abd592b8d5d166d69095c87323edc8f03b6
Raviranjan Kumar <2311201273@stu.manit.ac.in>
2026-05-18 18:16:03 +0530

    basic tcp server implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,49 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 int main() {
-    std::cout << "HTTP Server Starting..." << std::endl;
+    // Create socket
+    int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (serverSocket == -1) {
+        std::cerr << "Failed to create socket\n";
+        return 1;
+    }
+
+    std::cout << "Socket created successfully\n";
+
+    // Define server address
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(8080);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket
+    if (bind(serverSocket,
+             (struct sockaddr*)&serverAddress,
+             sizeof(serverAddress)) < 0) {
+
+        std::cerr << "Bind failed\n";
+        return 1;
+    }
+
+    std::cout << "Bind successful on port 8080\n";
+
+    // Listen for connections
+    if (listen(serverSocket, 5) < 0) {
+        std::cerr << "Listen failed\n";
+        return 1;
+    }
+
+    std::cout << "Server listening on port 8080...\n";
+
+    // Keep server alive
+    while (true) {
+    }
+
+    close(serverSocket);
+
     return 0;
 }
